0801c262a720b4385d34f91ed5b93d60d06497a1
Jules <1658895307@qq.com>
2020-05-20 18:18:38 +0800

    study FailureAnalyzer usage

---
+++ SpringDemo/src/main/java/com/vino/config/analyzer/IllegalArgumentFailureAnalyzer.java
@@ -0,0 +1,13 @@
+package com.vino.config.analyzer;
+
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+public class IllegalArgumentFailureAnalyzer extends AbstractFailureAnalyzer<IllegalArgumentException> {
+
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, IllegalArgumentException cause) {
+        return new FailureAnalysis(cause.getMessage(), "Please check startup argument!", cause);
+    }
+}
+
---
+++ SpringDemo/src/main/java/com/vino/config/analyzer/NullPointFailureAnalyzer.java
@@ -0,0 +1,17 @@
+package com.vino.config.analyzer;
+
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+public class NullPointFailureAnalyzer extends AbstractFailureAnalyzer<NullPointerException> {
+
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, NullPointerException cause) {
+        return new FailureAnalysis(cause.getMessage(), "请检查空指针", cause);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(String.valueOf(10).contains("1"));
+    }
+}
+

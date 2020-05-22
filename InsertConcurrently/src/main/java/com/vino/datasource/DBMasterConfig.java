4b229dfdde40bc007e323824b20a28ace5d24dbf
Jules <1658895307@qq.com>
2020-05-22 18:25:23 +0800

    develop InsertConcurrently function

---
+++ InsertConcurrently/src/main/java/com/vino/datasource/DBMasterConfig.java
@@ -0,0 +1,4 @@
+package com.vino.datasource;
+
+public class DBMasterConfig {
+}
---
+++ InsertConcurrently/src/main/java/com/vino/datasource/DBSlaveConfig.java
@@ -0,0 +1,41 @@
+package com.vino.datasource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+Configuration
+@MapperScan(basePackages = "com.vino.mapper.test1", sqlSessionTemplateRef  = "test1SqlSessionTemplate")
+public class DBSlaveConfig {
+
+    @Bean(name = "test1DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.test1")
+    @Primary
+    public DataSource testDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "test1SqlSessionFactory")
+    @Primary
+    public SqlSessionFactory testSqlSessionFactory(@Qualifier("test1DataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        return bean.getObject();
+    }
+
+    @Bean(name = "test1TransactionManager")
+    @Primary
+    public DataSourceTransactionManager testTransactionManager(@Qualifier("test1DataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "test1SqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate testSqlSessionTemplate(@Qualifier("test1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}

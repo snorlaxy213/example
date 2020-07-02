fe06158e36d10a42bd41e74fcff691498160c5ef
Jules <1658895307@qq.com>
2020-07-02 18:39:03 +0800

    完成分布式ID的设计

--- MultipleDataSource/src/main/java/com/vino/controller/DistributeController.java
+++ MultipleDataSource/src/main/java/com/vino/controller/DistributeController.java
@@ -2,6 +2,7 @@ package com.vino.controller;
 
 import java.util.UUID;
 
+import com.vino.util.SnowflakeIdWorker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +17,18 @@ public class DistributeController {
      * 优点:
      * 容易实现，产生快
      * ID唯一(几乎不会产生重复id)
-     * 无需中心化的服务器
+     * 无需中心化的服务器(本地生成，不会基于数据库)
      * 不会泄漏商业机密
      *
      * 缺点:
      * 可读性差
-     * 占用空间太多(16个字节)
+     * UUID太长(16个字节)，占用空间太多 ，作为主键性能很差；
      * 影响数据库的性能, 比如UUID or GUID as Primary Keys? Be Careful!
+     * --UUID不具有有序性，会导致 B+ 树索引在写的时候有过多的随机写操作（连续的 ID 可以产生部分顺序写）；
      *
+     * 适用场景：文件名、编号。。。。
      */
-    @GetMapping(name = "uuid")
+    @GetMapping(value = "uuid")
     public String UUID() {
         return UUID.randomUUID().toString();
     }
@@ -48,10 +51,27 @@ public class DistributeController {
      * 使用方法：
      * Mysql：设置每个数据库递增的起始值和步长（步长为服务数据库的个数）
      * Oracle： 设置每个Sequence的起始值和步长（步长为服务数据库的个数）
+     * 适用场景：在用户防止产生的 ID 重复时，这种方案实现起来比较简单，也能达到性能目标。但是服务节点固定，步长也固定，将来如果还要增加服务节点，就不好搞了。
      *
      */
 
-
-
-
+    /**
+     * snowflake 算法是 twitter 开源的分布式 id 生成算法，采用 Scala 语言实现，是把一个 64 位的 long 型的 id，
+     * 1 个 bit 是不用的，用其中的 41 bits 作为毫秒数，用 10 bits 作为工作机器 id，12 bits 作为序列号。
+     *
+     * 优点：
+     * 存储少, 8个字节
+     * 可读性高
+     * 性能好，可以中心化的产生ID,也可以独立节点生成
+     *
+     * 缺点：
+     * 时间回拨会重复产生ID
+     * ID生成有规律性,信息容易泄漏
+     *
+     */
+    @GetMapping(value = "snowFlakeId")
+    public Long snowFlakeId() {
+        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
+        return idWorker.nextId();
+    }
 }

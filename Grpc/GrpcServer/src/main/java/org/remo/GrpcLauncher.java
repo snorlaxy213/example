798c752a0e8ef490a41357475c5213da070e1334
Jules <iso2ez4vino@outlook.com>
2021-03-02 17:41:21 +0800

    Grpc

--- Grpc/GrpcServer/src/main/java/org/remo/GrpcLauncher.java
+++ Grpc/GrpcServer/src/main/java/org/remo/GrpcLauncher.java
@@ -5,7 +5,6 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;

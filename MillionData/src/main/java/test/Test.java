7e92573341c46e9fc73f9b3d007068a5c94a2448
Grio Vino <iso2ez4vino@outlook.com>
2021-09-03 00:31:11 +0800

    Million Data Parse - Excel事件驅動模式加載數據

--- MillionData/src/main/java/test/Test.java
+++ MillionData/src/main/java/test/Test.java
@@ -1,36 +1,63 @@
 package test;
 
 import model.User;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+import parser.ExcelEventParser;
 import util.CsvUtil;
 import util.ExcelUtil;
 
-import java.io.*;
-import java.text.DateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
 
+    private static final String excelPath = "D:\\Space\\Java\\DevelopDemo\\MillionData\\src\\main\\resources\\other\\进货清单.xlsx";
+    private static final String csvPath = "C:\\Users\\iso2e\\Desktop\\sys_user_202108312355.csv";
+
     public static void main(String[] args) {
-        csvRead();
+//        csvRead();
+//        excelRead();
+        eventModeExcelRead();
     }
 
     private static void excelRead() {
         long start = System.currentTimeMillis();
-        List<User> users = ExcelUtil.importExcel(new File("D:\\Space\\Java\\DevelopDemo\\MillionData\\src\\main\\resources\\other\\进货清单.xlsx"));
+        List<User> users = ExcelUtil.importExcel(new File(excelPath));
         long end = System.currentTimeMillis();
         System.out.println("解析Excel花费时间：" +(end - start) + "毫秒");
     }
 
+    private static void eventModeExcelRead() {
+        long start = System.currentTimeMillis();
+
+        final List<List<String>> table = new ArrayList<>();
+        new ExcelEventParser(excelPath).setHandler(new ExcelEventParser.SimpleSheetContentsHandler(){
+
+            private List<String> fields;
+
+            @Override
+            public void endRow(int rowNum) {
+                if(rowNum == 0){
+                    // 第一行中文描述忽略
+                }else if(rowNum == 1){
+                    // 第二行字段名
+                    fields = row;
+                }else {
+                    // 数据
+                    table.add(row);
+                }
+            }
+        }).parse();
+
+        long end = System.currentTimeMillis();
+
+        System.err.println(table.size());
+        System.err.println(end - start);
+    }
+
     private static void csvRead() {
         long start = System.currentTimeMillis();
-        List<User> users = CsvUtil.parseCsv(new File("C:\\Users\\iso2e\\Desktop\\sys_user_202108312355.csv"));
+        List<User> users = CsvUtil.parseCsv(new File(csvPath));
         long end = System.currentTimeMillis();
         System.out.println("解析Csv花费时间：" +(end - start) + "毫秒");
     }
